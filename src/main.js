02bab83c169086087d3e26bc813dad3db58507c0
Simon-Manasse <112821091+Simon-Manasse@users.noreply.github.com>
2024-03-20 10:20:47 +0100

    Added favicon and updated the navbar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
-export const init = ()=>{
+const gradient = [
+    "#3d97a8", // gray 500
+    "#69a2bf", // gray 400
+    "#b3bad9", // gray 300
+    "#e5d9e7", // gray 200
+    "white", // white
+    "#e5d9e7", // gray 200
+    "#b3bad9", // gray 300
+    "#69a2bf", // gray 400
+    "#3d97a8", // gray 500
+];
+
+const size = [
+    "14px",
+    "16px",
+    "18px",
+    "20px",
+    "30px",
+    "20px",
+    "18px",
+    "16px",
+    "14px"
+]
+
+const opacity = [
+    "0.3",
+    "0.5",
+    "0.6",
+    "0.8",
+    "1",
+    "0.8",
+    "0.6",
+    "0.5",
+    "0.3"
+]
+
+
+
+export const init = () => {
     const coords = { x: 0, y: 0 };
     const circles = document.querySelectorAll(".circle");
 
@@ -65,27 +103,38 @@ export const init = ()=>{
     animateCircles();
 };
 
-export const navbarAnimation=()=>{
+export const navbarAnimation = () => {
 
-    const gradient = [
-        "#6b7280", // gray 500
-        "#9ca3af", // gray 400
-        "#d1d5db", // gray 300
-        "#e5e7eb", // gray 200
-        "white", // white
-        "#e5e7eb", // gray 200
-        "#d1d5db", // gray 300
-        "#9ca3af", // gray 400
-        "#6b7280", // gray 500
-    ];
-let dots = document.getElementsByClassName('navBar');
-for (let item of dots){
-    item.addEventListener('click',()=>{
-       const id =  item.id
-      for (let i = 0; i < dots.length; i++) {
-        let gradientId = 5 + i - parseInt(id)
-       dots[i].firstChild.style.backgroundColor = gradient[gradientId];
-      }
-    })
-}
+    let dots = document.getElementsByClassName('navBar');
+    for (let item of dots) {
+        item.addEventListener('click', () => {
+            const id = item.id
+            for (let i = 0; i < dots.length; i++) {
+                navbarStyleChange(dots, id)
+            }
+        })
+    }
 };
+
+
+export function navbarScrollAnimation() {
+    let dots = document.getElementsByClassName('navBar');
+    window.onscroll = () => {
+        console.log(window.scrollY)
+        const thresholds = [935, 1870, 2805, 3720];
+        let index = thresholds.findIndex(threshold => window.scrollY < threshold);
+        navbarStyleChange(dots, index === -1 ? 5 : index + 1);
+    }
+}
+
+function navbarStyleChange(dots, id) {
+    for (let i = 0; i < dots.length; i++) {
+        let valueId = 5 + i - parseInt(id)
+        let div = dots[i].firstChild
+        div.style.transition = "all 0.7s";
+        div.style.backgroundColor = gradient[valueId];
+        div.style.height = size[valueId];
+        div.style.width = size[valueId];
+        div.style.opacity = opacity[valueId];
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
     import Card from "$lib/components/Card.svelte";
     import Example from "$lib/components/Example.svelte";
     import { onMount } from "svelte";
-    import { init, navbarAnimation } from "../main";
+    import { init, navbarAnimation, navbarScrollAnimation } from "../main";
 
     onMount(()=>{
     init();
     navbarAnimation()
+    navbarScrollAnimation()
     });
     
 </script>
@@ -37,22 +38,28 @@
 
     <!-- Navigation -->
 
-        <div class="  fixed z-50 h-screen  flex flex-col right-5 gap-4 justify-center float-right border-none">
-            <a href="#Home" id="1" class="navBar"><div class=" w-5 h-5  z-[555555] bg-white rounded-full"></div></a>
-            <a href="#Projects" id="2" class="navBar"><div class=" w-5 h-5  z-[555555] bg-gray-200 rounded-full"></div></a>
-            <a href="#Education" id="3" class="navBar"><div class=" w-5 h-5  z-[555555] bg-gray-300 rounded-full"></div></a>
-            <a href="#Skills" id="4" class="navBar"><div class=" w-5 h-5  z-[555555] bg-gray-400 rounded-full"></div></a>
-            <a href="#Hobbies" id="5" class="navBar"><div class=" w-5 h-5  z-[555555] bg-gray-500 rounded-full"></div></a>
+    <!-- "30px",
+    "20px",
+    "18px",
+    "16px",
+    "14px" -->
+
+        <div class="  fixed z-50 h-screen  flex flex-col right-5 gap-4 justify-center float-right border-none items-center">
+            <a href="#Home" id="1" class="navBar"><div class=" w-[30px] h-[30px]  z-[555555] bg-white rounded-full"></div></a>
+            <a href="#Projects" id="2" class="navBar"><div class=" w-[20px] h-[20px]  z-[555555] bg-[#e5d9e7] rounded-full"></div></a>
+            <a href="#Education" id="3" class="navBar"><div class=" w-[18px] h-[18px]  z-[555555] bg-[#b3bad9] rounded-full"></div></a>
+            <a href="#Skills" id="4" class="navBar"><div class=" w-[16px] h-[16px]  z-[555555] bg-[#69a2bf] rounded-full"></div></a>
+            <a href="#Hobbies" id="5" class="navBar"><div class=" w-[14px] h-[14px]  z-[555555] bg-[#3d97a8] rounded-full"></div></a>
         </div>
     
-    <div class="">
+    <div class="  ">
         <section class="ScrollPics" id="Home">
             <!-- SC=ScrollPics -->
             <div class="SC_inner" id="section_0">
                 <figure></figure>
                 <h1 class="SC_title z-10">Welcome to my webpage</h1>
-                <img class="h-[30%] flex fixed right-[42%]" src="/src/img/canvalogo.png" alt="">
-                <!-- <img class="bottom-0 right-[38%] flex fixed" src="/src/img/wave.gif" alt=""> -->
+                <img id="logo" class="h-[30%] flex fixed right-[42%]" src="/src/img/canvalogo.png" alt="">
+                <p class="SC_content justify-center items-center text-4xl mt-[20rem]">Created by Simon Manassé</p>
             </div>
         </section>
         <section class="ScrollPics" id="Projects">
