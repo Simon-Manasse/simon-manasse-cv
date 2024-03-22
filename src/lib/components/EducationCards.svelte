0f19a7d553c998a82e051a378617054bdb732c81
Simon-Manasse <112821091+Simon-Manasse@users.noreply.github.com>
2024-03-22 16:21:15 +0100

    finished education page

---
+++ src/lib/components/EducationCards.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let id;
+    export let image;
+    export let title;
+    export let description;
+    export let classes;
+</script>
+
+<div id="{id}" class="{classes} SC_content perspectiveDiv  h-[40rem] w-[30rem] bg-white text-black flex flex-col">
+    <img src="src/img/{image}" class="mt-5 h-[10rem]" alt="">
+    <div  class="items-center flex flex-col my-7 text-xl ">
+        <h1 class=" justify-center items-center">{title}</h1>
+    </div>
+    <p class=" text-justify">{description}</div>
--- src/lib/components/pages/Education.svelte
+++ src/lib/components/pages/Education.svelte
@@ -1,18 +1,28 @@
+<script>
+    import EducationCards from "../EducationCards.svelte";
+
+</script>
 <section class="ScrollPics " id="Education">
     <!-- SC=ScrollPics -->
     <div class="SC_inner bg-zinc-800" id="section_2">
         <figure></figure>
         <h2 class="SC_title z-10 text-[8rem] text-white justify-center"><i>Education</i></h2>
-        <div id="hz" class="SC_content perspectiveDiv top-[8rem] left-[42rem] h-[40rem] w-[30rem] z-50 bg-white text-black flex flex-col">
-            <img src="src/img/hz.png" class="mt-5" alt="">
-            <div  class="items-center flex flex-col my-7 text-xl">
-                <h1 class=" justify-center items-center">HBO ICT HZ university of applied science</h1>
-            </div>
-            <p class=" text-justify">In the first year at HZ University, I learned foundational web development skills including HTML, CSS, and JavaScript, as well as TypeScript for building scalable applications. Additionally, I gained proficiency in PHP and Laravel for server-side development and learned about containerization using Docker.
-                In the second year, the curriculum expanded to cover advanced topics such as Svelte for building reactive user interfaces, Microservices architecture for developing scalable and resilient applications, and Data Science with Python for analyzing and interpreting large datasets to extract valuable insights.</p>
-            </div>
-        <div id="faller" class="SC_content perspectiveDiv top-[9rem] left-[42rem] h-[40rem] w-[30rem] z-40 bg-white text-black">Faller</div>
-
+        <EducationCards
+        id = "hz"
+        image="hz.png"
+        title="HBO ICT HZ university of applied science"
+        description="In the first year at HZ University, I learned foundational web development skills including HTML, CSS, and JavaScript, as well as TypeScript for building scalable applications. Additionally, I gained proficiency in PHP and Laravel for server-side development and learned about containerization using Docker.
+        In the second year, the curriculum expanded to cover advanced topics such as Svelte for building reactive user interfaces, Microservices architecture for developing scalable and resilient applications, and Data Science with Python for analyzing and interpreting large datasets to extract valuable insights."
+        classes="top-[8rem] left-[42rem] z-50"
+        />
+        <EducationCards 
+        id="faller"
+        image="faller.png"
+        title="Pápai SZC Faller Jenő Technikum"
+        description="
+        During my four years in high school, I specialized in IT, gaining expertise in various areas. I acquired skills in application development using C# and .NET framework, along with proficiency in web development encompassing HTML, JavaScript, CSS, and relevant frameworks. Additionally, I mastered the art of structuring and managing MySQL databases, adept in executing queries for efficient data retrieval. Furthermore, I delved into the Cisco ecosystem, honing my abilities in network design and configuration to ensure optimal security and functionality."
+        classes="top-[9rem] left-[42rem] z-40"
+        />
         <div>
             <div id="nextCard" class="SC_content rounded-full bg-white h-[5rem] w-[10rem] z-[60] items-center justify-center top-[50rem] left-[54rem] text-black">Next card</div>
         </div>
