80311e56ba5ac14c5cb63feb483e08da825170d5
Simon-Manasse <112821091+Simon-Manasse@users.noreply.github.com>
2024-04-24 15:33:29 +0200

    Updates

--- server/nodes/1.js
+++ server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ea406619.js","_app/immutable/chunks/index.8528c4bc.js","_app/immutable/chunks/singletons.dddd13d3.js"];
+export const imports = ["_app/immutable/nodes/1.b903bd62.js","_app/immutable/chunks/index.8528c4bc.js","_app/immutable/chunks/singletons.b72c040a.js"];
 export const stylesheets = [];
 export const fonts = [];
